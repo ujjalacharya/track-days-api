674d733845ff46d4f8a83c26ea7a4baf4912ed3e
Ujjal Acharya <acharyaujjal1@gmail.com>
2025-01-28 13:10:01 +0100

    Validate .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { TasksModule } from './apps/tasks/tasks.module';
 import { PerformedTasksModule } from './apps/performed_tasks/tasks/performed_tasks.module';
 import { UsersModule } from './apps/users/users.module';
 import { AuthModule } from './apps/auth/auth.module';
+import { validate } from './config/config.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+      validate,
     }),
     DatabaseModule,
     TasksModule,
---
+++ src/config/config.schema.ts
@@ -0,0 +1,30 @@
+import { IsString, IsNumber } from 'class-validator';
+
+export class EnvironmentVariables {
+  @IsString()
+  DB_NAME: string;
+
+  @IsNumber()
+  DB_PORT: number;
+
+  @IsString()
+  DB_USER: string;
+
+  @IsString()
+  DB_PASS: string;
+
+  @IsString()
+  DB_ADDR: string;
+
+  @IsString()
+  GOOGLE_CLIENT_ID: string;
+
+  @IsString()
+  GOOGLE_CLIENT_SECRET: string;
+
+  @IsString()
+  GOOGLE_CALLBACK_URL: string;
+
+  @IsString()
+  JWT_SECRET: string;
+}
